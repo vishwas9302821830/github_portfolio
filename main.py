3b7e085eda6e63abbb1552d8b8d86efc4b9c9efe
Vishwas Sharma <vishwas9302821830@gmail.com>
2026-09-03 22:02:34 +0530

    Update Heading

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import gitguard.analyzer as git
 
-print("GitGuard Repository Report")
+print("GitGuard Repository Analyzer")
 print("----------------------------")
 
 
@@ -8,18 +8,18 @@ repository = git.get_repository_name()
 branch = git.get_current_branch()
 commits = git.get_commit_count()
 status = git.get_working_tree_status()
-branches=git.get_all_branches()
-recent_commits=git.get_recent_commits()
-print("Repository Name:",repository)
+branches = git.get_all_branches()
+recent_commits = git.get_recent_commits()
+print("Repository Name:", repository)
 print()
 print("Current Branch:", branch)
 print()
 print("Total Commits:", commits)
 print()
-print("Working Tree Status:",status)
+print("Working Tree Status:", status)
 print()
-print(f'''Branches:
-{branches}''')
+print(f"""Branches:
+{branches}""")
 print()
 print(f"""Recent Commits:
 {recent_commits}""")
