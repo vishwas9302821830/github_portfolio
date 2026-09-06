80fe1dbfde78b75f56fd16f63791de77968ac2ba
Vishwas Sharma <vishwas9302821830@gmail.com>
2026-09-06 19:20:10 +0530

    Add score and recommendation system

--- gitguard/analyzer.py
+++ gitguard/analyzer.py
@@ -20,7 +20,6 @@ def get_commit_count():
 
 
 def get_working_tree_status():
-
     result = run_git_command("git status --porcelain")
     if result == "":
         return  "Clean"
@@ -29,17 +28,72 @@ def get_working_tree_status():
 
 
 def get_repository_name():
-
     path = run_git_command("git rev-parse --show-toplevel")
     repo = path.split("/")[-1]
-
     return  repo
 
 def get_all_branches():
     branches=run_git_command("git branch")
     return branches
 
+def get_branch_count():
+    no_of_branches=len(get_all_branches().splitlines())
+    return no_of_branches
+
 
 def get_recent_commits():
     commits=run_git_command("git log --oneline")
     return commits
+
+
+def score():
+    branch = get_current_branch()
+    commits = get_commit_count()
+    count_branches= get_branch_count()
+    status = get_working_tree_status()
+    recent_commits = get_recent_commits()
+    total_score=0
+    if status=="Clean":
+        total_score+=30
+    if commits>=5:
+        total_score+=30
+    elif commits == 3 or commits == 4:
+        total_score += 20
+    elif commits== 2 or commits== 1:
+        total_score += 10
+    if count_branches>=3:
+        total_score+=20
+    elif count_branches==2:
+        total_score+=10
+    elif count_branches==1:
+        total_score+=5
+    if recent_commits!="":
+        total_score+=10
+    if branch!="":
+        total_score+=10
+    return total_score
+
+def score_status(health_score):
+    if health_score>=90:
+        return "Excellent"
+    elif health_score>=75:
+        return "Good"
+    elif health_score>=50:
+        return "Better"
+    else:
+        return "Try to improve"
+
+def get_recommendations():
+    if get_working_tree_status()=="Dirty":
+        yield "Commit your pending changes"
+        if get_commit_count()<=4:
+            yield "Make more meaningful commits"
+    if get_branch_count()==1:
+        yield "Create/Use feature branches"
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -9,6 +9,11 @@ commits = git.get_commit_count()
 status = git.get_working_tree_status()
 branches = git.get_all_branches()
 recent_commits = git.get_recent_commits()
+branch_count=git.get_branch_count()
+health_score=git.score()
+score_level=git.score_status(health_score)
+recommendation=git.get_recommendations()
+
 
 
 print("Repository Name:",repository)
@@ -23,9 +28,22 @@ print()
 print("Working Tree Status:", status)
 print()
 
+print("Total no. of Branches:",branch_count)
+print()
+
 print(f'''Branches:
 {branches}''')
-
 print()
+
 print(f"""Recent Commits:
 {recent_commits}""")
+print()
+
+print("Score:",health_score)
+print()
+print("Score Status:",score_level)
+print()
+
+print("Recommendations:")
+for recommend in recommendation:
+    print(recommend)
